e7416648bf4d229a6ce767ea4f5bd79180daf777
Malo <maloandre39@gmail.com>
2023-01-24 00:20:33 +0100

    update, mentions légales

--- js/index.js
+++ js/index.js
@@ -6,14 +6,3 @@ menuHamburger.addEventListener('click', () => {
     navLinks.classList.toggle('mobile-menu')
     navLinks.classList
 })
-
-function discord_card() {
-    const element = document.querySelector('.iframe_discord');
-    element.classList.add('iframe_discord_visible');
-    setTimeout(discord_card_undo, 5000);
-}
-
-function discord_card_undo() {
-    const element = document.querySelector('.iframe_discord');
-    element.classList.remove('iframe_discord_visible');
-}
