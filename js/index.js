b52cffcaceaa6fe174b8acc638179a955a546b85
Malo <maloandre39@gmail.com>
2023-02-19 15:36:20 +0100

    amélioration

--- js/index.js
+++ js/index.js
@@ -6,3 +6,22 @@ menuHamburger.addEventListener('click', () => {
     navLinks.classList.toggle('mobile-menu')
     navLinks.classList
 })
+
+
+window.onscroll = function () { scrollNavbar() };
+function scrollNavbar() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("navbar").style.padding = "10px 25px";
+        document.getElementById("navbar").style.backdropFilter = "blur(3px)";
+        document.getElementById("navbar").style.background = "linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0.15) 50%, rgba(255, 255, 255, 0) 100%)";
+    } else {
+        document.getElementById("navbar").style.padding = "50px";
+        document.getElementById("navbar").style.backdropFilter = "none";
+        document.getElementById("navbar").style.background = "none";
+    }
+}
+
+function scrollClick() {
+    // let pageHeight = window.innerHeight;
+    window.scrollTo(0, 804);
+}
